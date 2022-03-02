3b8b7a3fa5400ec1915fa857e8f64ecb7e820a81
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-03-02 17:06:49 +0300

    Add sign out logic

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { response } from "express";
 
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Hi there");
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRoute };
