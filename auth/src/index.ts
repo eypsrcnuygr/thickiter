12dd9c6001f007c7396a7e8b2e1e6b39fc458908
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-03-02 13:20:03 +0300

    Add jwt and bind it with cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,9 +7,18 @@ import { signoutRoute } from "./routes/signout";
 import { signupRoute } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handlers";
 import { NotFoundError } from "./errors/not-found-error";
+import cookieSession from "cookie-session";
 
 const app = express();
+// The app is proxied via ingress-nginx so we add this set-up
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRoute);
@@ -25,6 +34,10 @@ app.all("*", async (req, res, next) => {
 app.use(errorHandler);
 
 const startup = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be initialized!");
+  }
+
   try {
     console.log("Connected to MongoDB!!");
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth-thickiter");
