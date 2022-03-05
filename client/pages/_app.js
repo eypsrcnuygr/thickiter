ffc75c18399a2f9dbffaa81f7fd993ceb5cafea3
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-03-06 00:13:32 +0300

    Finish the auth for client, add signout

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,29 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-const _App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const _App = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+_App.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/current_user");
+
+  // To use getInitialProps inside the page components, right now app component prevent this without the conf below!
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return {
+    pageProps,
+    ...data,
+  };
 };
 
 export default _App;
