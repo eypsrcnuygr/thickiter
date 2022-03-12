fb6b532fa70566c5a80178a3aba1e47bc708fbf1
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-03-13 02:55:41 +0300

    Add types

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,12 @@
 import { Listener } from "./base-listener";
 import { Message } from "node-nats-streaming";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = "payments-service";
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message): void {
     console.log("Event data!", data);
 
     msg.ack();
