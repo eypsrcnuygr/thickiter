7be194f70090a7fe9241680199180142866158c9
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-03-13 02:34:12 +0300

    Make abstract class for listener, subclass it

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 const stan = nats.connect("thickiter", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
@@ -12,27 +13,8 @@ stan.on("connect", () => {
     console.log("Nats connection closed!");
     process.exit();
   });
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("orders-service");
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    options
-  );
 
-  subscription.on("message", (msg: Message) => {
-    console.log("Message received!");
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Received event ${msg.getSequence()} with data ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
