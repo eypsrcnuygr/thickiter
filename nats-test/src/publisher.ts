271e7a3a1f123e69c9b81d1b55631e2bd4c8610a
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-03-13 03:10:51 +0300

    Add types for publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 const stan = nats.connect("thickiter", "abc", {
   url: "http://localhost:4222",
@@ -7,13 +8,20 @@ const stan = nats.connect("thickiter", "abc", {
 stan.on("connect", () => {
   console.log("Publisher connected to nats!");
 
-  const data = JSON.stringify({
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
     id: "123",
-    title: "concert",
+    title: "concert super",
     price: 20,
   });
 
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published!");
-  });
+  // const data = JSON.stringify({
+  //   id: "123",
+  //   title: "concert",
+  //   price: 20,
+  // });
+
+  // stan.publish("ticket:created", data, () => {
+  //   console.log("Event published!");
+  // });
 });
