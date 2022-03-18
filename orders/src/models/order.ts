61756791ca4f0f76aee854a599764d99183d3b99
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-03-18 20:41:38 +0300

    Add order model

---
+++ orders/src/models/order.ts
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@esuthickiter/common";
+import { TicketDoc } from "./ticket";
+
+interface OrderAttributes {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attributes: OrderAttributes): OrderDoc;
+}
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+OrderSchema.statics.build = (attributes: OrderAttributes) => {
+  return new Order(attributes);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", OrderSchema);
+
+export { Order };
